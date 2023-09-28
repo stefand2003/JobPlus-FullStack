f2c2298c05a1fa43cbc3af833dc4de46eb7efb45
stefand2003 <119484929+stefand2003@users.noreply.github.com>
2023-09-28 06:58:58 +0100

    LoginPage completed

--- FrontEnd/src/components/login/login.jsx
+++ FrontEnd/src/components/login/login.jsx
@@ -1,27 +1,75 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
 import '../styles/form.scss';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { parseErrors } from '../../utils/parseErrors';
+import Alert from '../../alert/alert';
+import { useNavigate } from 'react-router-dom';
 
 export default function login() {
+  const [identifier, setIdentifier] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [alert, setAlert] = useState({});
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const data = {
+      identifier,
+      password,
+    };
+
+    try {
+      const res = await axios.post(
+        'http://localhost:1337/api/auth/local',
+        data
+      );
+
+      setIdentifier('');
+      setPassword('');
+
+      navigate('/');
+    } catch (err) {
+      setAlert(parseErrors(err));
+    }
+  };
+
   return (
-    <div class="form form--page">
-      <div class="form__group form__group--page">
-        <label class="form__label">Email</label>
-        <input class="form__field" type="text" placeholder="Email" />
-      </div>
+    <>
+      <Alert data={alert} />
+      <form class='form form--page' onSubmit={handleSubmit}>
+        <div class='form__group form__group--page'>
+          <label class='form__label'>Email</label>
+          <input
+            class='form__field'
+            type='text'
+            placeholder='Email'
+            value={identifier}
+            onChange={(e) => setIdentifier(e.target.value)}
+          />
+        </div>
 
-      <div class="form__group form__group--page">
-        <label class="form__label">Password</label>
-        <input class="form__field" type="text" placeholder="Password" />
-      </div>
+        <div class='form__group form__group--page'>
+          <label class='form__label'>Password</label>
+          <input
+            class='form__field'
+            type='password'
+            placeholder='Password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
 
-      <div class="form__group form__group--page">
-        <input class="form__btn" type="submit" value="Login" />
-      </div>
+        <div class='form__group form__group--page'>
+          <input class='form__btn' type='submit' value='Login' />
+        </div>
 
-      <footer>
-        Dont have an account? <Link to='/register'>Register</Link>
-      </footer>
-    </div>
+        <footer>
+          Dont have an account? <Link to='/register'>Register</Link>
+        </footer>
+      </form>
+    </>
   );
 }
